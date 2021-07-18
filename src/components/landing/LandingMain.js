97d0871c9768b884a45c480fa6ecb4b7b49714bd
ben <ejrrlabc@gamil.com>
2021-07-18 20:06:11 +0900

    :lipstick: update a layout of landing
    update a layout of landing

--- src/components/landing/LandingMain.js
+++ src/components/landing/LandingMain.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
-import background from "../../images/landing.mp4";
+import { Icon } from "react-icons-kit";
+import { pencilSquareO } from "react-icons-kit/fa/pencilSquareO";
+import { ic_palette } from "react-icons-kit/md/ic_palette";
+import { users } from "react-icons-kit/fa/users";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import LandingThird from "./LandingThird";
@@ -10,6 +13,7 @@ import {
   MainInnerWrapper,
   MainInnerArticle,
   VideoContainer,
+  SecondSectionHeader,
 } from "../../styles/landing/LandingMain.style";
 
 const LandingMain = () => {
@@ -21,9 +25,36 @@ const LandingMain = () => {
     <MainWrapper id="mainsection">
       <MainDivForColor>
         <MainInnerSection>
+          <SecondSectionHeader data-aos={"fade-down"} data-aos-duration={"800"}>
+            오늘의 주요 기능
+          </SecondSectionHeader>
           <MainInnerWrapper>
-            <MainInnerArticle>
-              <h2 data-aos={"fade-right"} data-aos-duration={"800"}>
+            <MainInnerArticle data-aos={"fade-right"} data-aos-duration={"800"}>
+              <div>
+                <h2>
+                  <Icon icon={pencilSquareO} size={25} /> 달력 클릭 후 일기 작성
+                </h2>
+                <p>
+                  원하는 날짜를 클릭하여 그 날의 일기를 작성하실 수 있습니다 .
+                </p>
+              </div>
+              <div>
+                <h2>
+                  <Icon icon={ic_palette} size={25} /> 여러 가지 기능으로 하루를
+                  표현
+                </h2>
+                <p>
+                  사진 업로드 및 그림판 기능과 노래를 선택하여 오늘의 일기를
+                  풍부하게 표현해보세요 .
+                </p>
+              </div>
+              <div>
+                <h2>
+                  <Icon icon={users} size={25} /> 일기 공유
+                </h2>
+                <p>타인의 하루를 보고 공감 버튼을 눌러 보세요 .</p>
+              </div>
+              {/* <h2 data-aos={"fade-right"} data-aos-duration={"800"}>
                 음악, 그림이
                 <br />
                 함께하는 일기
@@ -38,14 +69,22 @@ const LandingMain = () => {
                 부드러운 글과 음악 <br />
                 그리고 자유롭게 그린 그림으로 <br />
                 '오늘'에 담아주세요 .
-              </p>
+              </p> */}
             </MainInnerArticle>
             <VideoContainer
               className="video-container"
               data-aos={"fade-left"}
               data-aos-duration={"500"}
             >
-              <video src={background} muted autoPlay loop playsInline></video>
+              <img
+                src={
+                  "https://user-images.githubusercontent.com/77098060/126061940-83ac21bc-9a61-4dd1-bf26-d3bba3495f5e.gif"
+                }
+                muted
+                autoPlay
+                loop
+                playsInline
+              ></img>
             </VideoContainer>
           </MainInnerWrapper>
         </MainInnerSection>
